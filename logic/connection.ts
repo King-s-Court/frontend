7c578186f4f1a49724a3e839f9fa3d5eea9343e6
Jasper van den Meiracker <jasper.vanden.meiracker@indicia.nl>
2023-05-22 14:24:52 +0200

    frontend-24: Commented connection

--- logic/connection.ts
+++ logic/connection.ts
@@ -2,35 +2,35 @@ import {HttpTransportType, HubConnectionBuilder, LogLevel} from "@microsoft/sign
 import {useState} from "react";
 
 async function Connection() {
-  const [connection, setConnection] = useState<HubConnectionBuilder>(null);
-
-  async function buildConnection() {
-    let connection = new HubConnectionBuilder()
-      .withUrl("https://localhost:7135/chessHub", {
-        skipNegotiation: true,
-        transport: HttpTransportType.WebSockets
-      })
-      .configureLogging(LogLevel.Information)
-      .withAutomaticReconnect()
-      .build();
-
-    connection.on("ReceiveMove", receiveMove);
-  }
-
-  async function sendMove(move, gameId) {
-    if (connection) {
-      await connection.invoke("SendMove", {move, gameId});
-    }
-  }
-  async function joinGame() {
-    if (connection) {
-      await connection.invoke("JoinGame", {gameId});
-    }
-  }
-  async function receiveMove(move) {
-    // Handle received move here
-    console.log("Received move: ", move);
-  }
+  // const [connection, setConnection] = useState<HubConnectionBuilder>(null);
+  //
+  // async function buildConnection() {
+  //   let connection = new HubConnectionBuilder()
+  //     .withUrl("https://localhost:7135/chessHub", {
+  //       skipNegotiation: true,
+  //       transport: HttpTransportType.WebSockets
+  //     })
+  //     .configureLogging(LogLevel.Information)
+  //     .withAutomaticReconnect()
+  //     .build();
+  //
+  //   connection.on("ReceiveMove", receiveMove);
+  // }
+  //
+  // async function sendMove(move, gameId) {
+  //   if (connection) {
+  //     await connection.invoke("SendMove", {move, gameId});
+  //   }
+  // }
+  // async function joinGame() {
+  //   if (connection) {
+  //     await connection.invoke("JoinGame", {gameId});
+  //   }
+  // }
+  // async function receiveMove(move) {
+  //   // Handle received move here
+  //   console.log("Received move: ", move);
+  // }
 }
 
 export default Connection;
